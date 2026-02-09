90228dd77cf7ed808c874ec1929f98fb1618cac4
Sarsela <an.kurnosova@gmail.com>
2026-02-09 07:56:26 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -10,38 +10,43 @@ const genDiff = (file1, file2) => {
   const keys2 = Object.keys(data2);
 
   keys1.forEach(key => {
-  if (!allKeys.includes(key)) {
-    allKeys.push(key);
-  }
-});
-
-  keys2.forEach(key => {
-  if (!allKeys.includes(key)) {
-    allKeys.push(key);
-  }
-});
-
-  allKeys.sort();
-
-  const resultLines = [];
-
-   allKeys.forEach(key => {
-    const inFirst = key in data1;     
-    const inSecond = key in data2;    
-    const value1 = data1[key];        
-    const value2 = data2[key];
-
-     if (!inFirst) { 
-      resultLines.push(`+ ${key}: ${value2}`);
-    } else if (!inSecond) {
-      resultLines.push(`- ${key}: ${value1}`);
-    } else if (value1 === value2) {
-      resultLines.push(`  ${key}: ${value1}`);
-    } else {
-      resultLines.push(`- ${key}: ${value1}`);
-      resultLines.push(`+ ${key}: ${value2}`);
+    if (!allKeys.includes(key)) {
+      allKeys.push(key);
     }
   });
+
+  keys2.forEach(key => {
+    if (!allKeys.includes(key)) {
+      allKeys.push(key);
+    }
+  });
+
+  allKeys.sort();
+  
+  const resultLines = [];
+  
+  allKeys.forEach(key => {
+    const inFirst = key in data1;
+    const inSecond = key in data2;
+    const value1 = data1[key];
+    const value2 = data2[key];
+    
+    if (!inFirst) {
+      
+      resultLines.push(`  + ${key}: ${value2}`);  
+    } else if (!inSecond) {
+      
+      resultLines.push(`  - ${key}: ${value1}`);  
+    } else if (value1 === value2) {
+      
+      resultLines.push(`    ${key}: ${value1}`);  
+    } else {
+      
+      resultLines.push(`  - ${key}: ${value1}`);  
+      resultLines.push(`  + ${key}: ${value2}`);  
+    }
+  });
+  
   return `{\n${resultLines.join('\n')}\n}`;
 };
 
