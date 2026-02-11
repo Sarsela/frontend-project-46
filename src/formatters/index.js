b41592f6577940ce12d057844f27996d4b514e74
Sarsela <an.kurnosova@gmail.com>
2026-02-11 09:29:22 +0700

    Add files via upload

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,17 +1,17 @@
-import normal from './normal.js'
+import stylish from './stylish.js'
 import plain from './plain.js'
 import json from './json.js'
 
 const formatters = {
-  normal,
+  stylish,
   plain,
   json,
-}
+};
 
-export default (tree, formatName = 'normal') => {
-  const formatter = formatters[formatName]
+export default (tree, formatName = 'stylish') => {
+  const formatter = formatters[formatName];
   if (!formatter) {
-    throw new Error(`Unknown format: ${formatName}`)
+    throw new Error(`Unknown format: ${formatName}`);
   }
-  return formatter(tree)
-}
+  return formatter(tree);
+};
